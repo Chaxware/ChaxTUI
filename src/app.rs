87c4fd690685e96273ba6d7991fdd0c14ad9c32f
Shamblonaut <shaheembn@gmail.com>
2024-05-22 23:20:24 +0530

    Implement basic Chat UI

--- src/app.rs
+++ src/app.rs
@@ -6,7 +6,7 @@ use crate::events::handle_events;
 use crate::ui::draw_ui;
 
 pub enum CurrentScreen {
-    DM,
+    Chat,
 }
 
 #[derive(Eq, PartialEq)]
@@ -15,24 +15,44 @@ pub enum AppState {
     Exit,
 }
 
+pub enum ChatType {
+    DM,
+    // Group,
+    // Server,
+}
+pub struct Chat {
+    pub chat_type: ChatType,
+    pub typing_message: String,
+}
+
 pub struct App {
     pub app_state: AppState,
     pub current_screen: CurrentScreen,
+    pub active_chat: Chat,
 }
 
 impl App {
     pub fn new() -> App {
         App {
             app_state: AppState::Active,
-            current_screen: CurrentScreen::DM,
+            current_screen: CurrentScreen::Chat,
+            active_chat: Chat {
+                chat_type: ChatType::DM,
+                typing_message: String::new(),
+            },
         }
     }
 
     pub fn run<B: Backend>(&mut self, terminal: &mut Terminal<B>) -> Result<()> {
         while self.app_state != AppState::Exit {
-            terminal.draw(draw_ui)?;
+            terminal.draw(|frame| draw_ui(frame, self))?;
             handle_events(self)?;
         }
         Ok(())
+    }
+
+    pub fn send_message(&self, message: &str) {
+        println!("{}", message);
+        // TODO: Implement (at least a fake) message send
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,35 +1,50 @@
 use ratatui::{
-    layout::{Alignment, Constraint, Direction, Layout},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style},
     text::Line,
-    widgets::{block::Title, Block, BorderType, Borders, Padding, Paragraph},
+    widgets::{Block, BorderType, Borders, Padding, Paragraph},
     Frame,
 };
 
-pub fn draw_ui(frame: &mut Frame) {
+use crate::app::App;
+
+pub fn draw_ui(frame: &mut Frame, app: &mut App) {
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
-            Constraint::Ratio(1, 3),
-            Constraint::Ratio(1, 3),
-            Constraint::Ratio(1, 3),
+            Constraint::Length(2),
+            Constraint::Min(3),
+            Constraint::Length(5),
         ])
         .split(frame.size());
-    let sub_chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Ratio(1, 3),
-            Constraint::Ratio(1, 3),
-            Constraint::Ratio(1, 3),
-        ])
-        .split(chunks[1]);
 
-    let greeting_block = Block::default()
-        .borders(Borders::ALL)
-        .border_type(BorderType::Rounded)
-        .title(Title::from(" Hello World ").alignment(Alignment::Center))
-        .padding(Padding::new(0, 0, sub_chunks[1].height / 2 - 1, 0));
-    let greeting = Paragraph::new(Line::from("Greetings, Stranger!"))
+    let hero = Paragraph::new("Chax")
         .centered()
-        .block(greeting_block);
-    frame.render_widget(greeting, sub_chunks[1]);
+        .block(Block::default().padding(Padding::new(0, 0, 1, 0)));
+    frame.render_widget(hero, chunks[0]);
+
+    let chat_window = Block::default()
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded);
+    frame.render_widget(chat_window, chunks[1]);
+
+    let mut typing_message = app.active_chat.typing_message.clone();
+    let mut empty_message = false;
+    if typing_message.is_empty() {
+        typing_message = String::from("Write a message...");
+        empty_message = true;
+    }
+
+    let message_box = Paragraph::new(Line::from(typing_message).style(if empty_message {
+        Style::default().fg(Color::DarkGray)
+    } else {
+        Style::default()
+    }))
+    .block(
+        Block::default()
+            .borders(Borders::ALL)
+            .border_type(BorderType::Rounded)
+            .padding(Padding::new(2, 0, 1, 0)),
+    );
+    frame.render_widget(message_box, chunks[2]);
 }
