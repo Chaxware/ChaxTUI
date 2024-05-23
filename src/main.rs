05e6d83b7d3ceeb28c1f04493f472ae754130755
Shamblonaut <shaheembn@gmail.com>
2024-05-23 23:42:01 +0530

    Add option to specify backend URL

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{stdout, Result};
+use std::{
+    env,
+    io::{stdout, Result},
+};
 
 use app::App;
 use crossterm::{
@@ -14,13 +17,21 @@ mod ui;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let mut backend_base_url: String = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "http://localhost:8787".into());
+
+    if backend_base_url.ends_with('/') {
+        backend_base_url.pop();
+    }
+
     enable_raw_mode()?;
     execute!(stdout(), EnterAlternateScreen)?;
 
-    let backend = CrosstermBackend::new(stdout());
-    let mut terminal = Terminal::new(backend)?;
+    let terminal_backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(terminal_backend)?;
 
-    let mut app = App::new();
+    let mut app = App::new(backend_base_url);
     app.run(&mut terminal).await?;
 
     disable_raw_mode()?;
